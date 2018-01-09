002a240815ae2bdb73416ddc4cfac045c3a7dcef
chris <chrisb000@gmail.com>
2018-01-09 15:39:41 -0800

    sync

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
 
 var app = express();
 var PORT = 3300;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", function(req, res) {
-    res.render('email', function (err, html) {
-        console.log(html);
-    })
+    res.send('hi')
+});
+
+app.post('/post', function(req, res) {
+    console.log("test");
 });
 
 app.listen(PORT, function() {
     console.log(`connected to ${PORT}`);
 });
+
+console.log("test");
